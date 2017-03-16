63b572530fc2c83328c4fc8d3bdc0682552787a6
Elias Tragas <etragas@hotmail.com>
2017-03-15 23:58:39 -0400

    More testing, added alternating optimization

--- Frameworkk/NMF_ATNN.py
+++ Frameworkk/NMF_ATNN.py
@@ -1,4 +1,5 @@
 import autograd.numpy as np
+from autograd.util import flatten
 from autograd import grad
 from NMF import NMF
 import time
@@ -33,7 +34,7 @@ class NMF_ATNN(NMF):
             regTerms += np.square(self.parameters[i][0]).sum() + np.square(self.parameters[i][1]).sum()
         regTerms += np.square(self.parameters[self.NET_DEPTH]).sum()
         keep = data > 0
-        loss = np.square(data- keep*self.inference(parameters)).sum() + .1*regTerms
+        loss = np.square(data- keep*self.inference(parameters,data=data)).sum() + regTerms
         return loss
 
 #Credit to David Duvenaud for sleek init code
@@ -44,28 +45,51 @@ class NMF_ATNN(NMF):
                  scale + scale *  rs.randn(n))      # bias vector
                 for m, n in zip(layer_sizes[:-1], layer_sizes[1:])]
 
-    def train_neural_net(self,alpha = .0003, max_iter = 1,latent_indices = None,data = None):
+    def train_neural_net(self,alpha = .00001, max_iter = 1,latent_indices = None,data = None):
         train_data = self.data if data is None else data
-        for iter in range(0,max_iter):
+        prev_grads = []
+        cur_grad = []
+        prev_grad_roll = None
+        alt = 0
+        for iter in range(0,10):
+            predicted_data = self.inference(self.parameters,train_data)
+            print("MSE is ",(abs(train_data-(train_data>0)*predicted_data).sum())/((train_data>0).sum()))
+            print(self.loss(self.parameters,self.data))
+
             start = time.time()
             print "before", start
             grads = grad(self.loss,0)(self.parameters, train_data)
             print "after", time.time() - start
             #Get gradients
             #Update parameters
-            for i in range (self.NET_DEPTH):
-                #Updating net weights
-                self.parameters[i] = [self.parameters[i][0] -alpha*grads[i][0], self.parameters[i][1] -alpha*grads[i][1]]
+            if not prev_grads:
+                prev_grads, grad_roll = flatten(grads)
+                prev_grads = grad_roll(prev_grads)
+            else:
+                a = flatten(grads)[0]
+                prev_grads = grad_roll(flatten(prev_grads)[0] * .95 + a)
+            cur_grad = prev_grads
+
+            if alt == 0:
+                for i in range (self.NET_DEPTH):
+                    #Updating net weights
+                    self.parameters[i] = [self.parameters[i][l] -alpha*cur_grad[i][l] for l in range(len(self.parameters[i]))]
+                print "net_pass"
+                alt = 1
+            else:
             #Updating col_latents
-            self.parameters[self.NET_DEPTH] += -alpha*grads[self.NET_DEPTH]
+                print "col_pass"
+                self.parameters[self.NET_DEPTH] += -alpha*cur_grad[self.NET_DEPTH]
+                alt = 0
+
 
     def neural_net_inference(self,parameters, data = None):
-            net_parameters = parameters[:2]
+            users, movies = data.shape
+            net_parameters = parameters[:3]
             colLatents = parameters[self.NET_DEPTH]
-
             temp_attention = []
             colLatents = parameters[self.NET_DEPTH]
-            for i in range(self.col_size):
+            for i in range(users):
               ratings = self.data[i,:].reshape([1,self.col_size]) #Data preproc
               ratings_high = ratings > 0 #Data preproc
               reduced_latents = colLatents[:,np.ndarray.flatten(ratings_high)] #Data prepoc
@@ -108,9 +132,11 @@ class NMF_ATNN(NMF):
         #Assume 3 layer net, so net_parameters
         W1 , b1 = net_parameters[0]
         W2, b2 = net_parameters[1]
+        W3, b3 = net_parameters[2]
         layer2 = relu(np.dot(input_data,W1) + b1)
         layer3 = relu(np.dot(layer2,W2) + b2)
-        return layer3
+        return relu(np.dot(layer3,W3) + b3)
+
 
 def softmax(x):
     #Compute softmax values for every element in input_data
--- Frameworkk/atnn.py
+++ Frameworkk/atnn.py
@@ -13,39 +13,49 @@ from NMF_ATNN import *
 full_data  = DataLoader().LoadData(file_path="../Data/ml-100k/u.data",data_type=DataLoader.MOVIELENS)
 
 #Reduce the matrix to toy size
-X = full_data[:100,:100]
+train, test = full_data[:400,:100], full_data[400:500,:100]
+X = train
 print(X)
 #Construct Model
 latent_size = 40
 
-model = NMF_ATNN(n_components=latent_size,data=X,layer_sizes=[latent_size+1, 80, 1])
+model = NMF_ATNN(n_components=latent_size,data=X,layer_sizes=[latent_size+1, 200,80, 1])
 model.inference = model.neural_net_inference
 model.loss = model.nnLoss
-for counter in range(0,200):
+for counter in range(0,100):
     model.train()
 
     print(counter)
     if (counter%1 == 0):
         #Mean square error is
-        predicted_data = model.inference(model.parameters)
-        print("MSE is ",(abs(X-predicted_data).sum())/(X.shape[0]*X.shape[1]))
+        predicted_data = model.inference(model.parameters,data=train)
+        print("MSE is ",(abs(X-(X>0)*predicted_data).sum())/((X>0).sum()))
         print(model.loss(model.parameters,model.data))
 
+        predicted_data = model.inference(model.parameters,data=test)
+        print("MSE Test is ",(abs(test-(test>0)*predicted_data).sum())/((test>0).sum()))
+        print(model.loss(model.parameters,test))
+
+
 #Do Inference
-invtrans = model.inference(model.parameters)
+invtrans = model.inference(model.parameters, data = train)
 print "new model"
 
 print(X)
 print(np.round(invtrans))
+print "with keeps"
 print(X*(X>0))
+print "inferred"
 print(np.round(invtrans) * (X > 0))
 
 #Test rowless on novel users.
-X = full_data[100:200,:100]
-model.data = X
+X = test
 print model.loss(model.parameters,X)/sum(X.shape)
 print(X)
+invtrans = model.inference(model.parameters,data=test)
 print(np.round(invtrans))
+print "with keeps"
 print(X*(X>0))
-print(np.round(invtrans) * (invtrans > 0))
+print "inferred"
+print(np.round(invtrans) * (X > 0))
 #Time for testing
