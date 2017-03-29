d1d22a1b0a9a0627b8a1615423a203b6f2f81cc8
Elias Tragas <etragas@hotmail.com>
2017-03-29 18:04:43 -0400

    Reverted

--- Framework/NMF_ATNN.py
+++ Framework/NMF_ATNN.py
@@ -3,11 +3,15 @@ from autograd import grad
 from NMF import NMF
 from autograd.util import flatten
 import time
+from utils import *
 from autograd.core import primitive
 from autograd.scipy.misc import logsumexp
 from autograd.optimizers import adam
+from joblib import Parallel, delayed
+import multiprocessing
 
 curtime = 0
+MAX_RECURSION = 4
 
 def lossGrad(data):
     return grad(lambda params,_: nnLoss(params,data=data))
@@ -53,7 +57,7 @@ def getInferredMatrix(parameters,data):
     return newarray
 
 
-def print_perf(params, iter=0, gradient=0, data = None):
+def print_perf(params, iter=0, gradient=[], data = None):
     """
     Prints the performance of the model
     """
@@ -62,6 +66,10 @@ def print_perf(params, iter=0, gradient=0, data = None):
     print "It took: {} s".format(time.time()- curtime)
     print("iter is ", iter)
     print("MSE is ",(abs(data-predicted_data).sum())/((data>0).sum()))
+    for key in gradient.keys():
+        x = gradient[key]
+        print key
+        print np.square(flatten(x)[0]).sum()/(len(flatten(x)[0])+10)
     print(loss(parameters=params,data=data))
     curtime = time.time()
 
@@ -73,33 +81,129 @@ def neural_net_inference(parameters,iter = 0, data = None):
     :param data: data to work on
     :return: A list of numpy arrays, each of which corresponds to a dense prediction of user ratings.
     """
-    net_parameters = parameters[:l1_size]
-    rating_net_parameters = parameters[l1_size:l2_size]
-
-    colLatents = parameters[l2_size]
-    attention_weight = parameters[l2_size+1]
     rating_predictions = [0]*data.shape[0]
 
     num_rows, num_columns = data.shape
-    for i in range(num_rows):
-        predictions = []
-        current_row = data[i,:]
+
+    #Go over each user
+    for user_index in range(num_rows):
+        current_row = data[user_index,:]
         rating_indices = flatten(current_row > 0)[0] #Only keep indices where the ratings are non-zero
         if rating_indices.sum() == 0:
-            rating_predictions[i] = np.array(0)
+            rating_predictions[user_index] = np.array(0) #In the case where no ratings exist, predict 0.
             continue
 
-        dense_latents = colLatents[:,rating_indices] #Grab latents for movied rated
-        dense_ratings = current_row[rating_indices].reshape((1,rating_indices.sum())) #Grab corresponding ratings
+        predictions = []
+        for rating_index in np.flatnonzero(current_row):
+            #For each index where a rating exists, generate it.
+            if rating_indices[rating_index] > 0:
+                predictions.append(recurrent_inference(parameters,iter,data,user_index,rating_index))
 
-        latents_with_ratings = np.concatenate((dense_latents,dense_ratings),axis = 0 ) #Append ratings to latents
-        prediction = neural_net_predict(net_parameters,np.transpose(latents_with_ratings)) #Feed through NN
-        row_latent = np.mean(prediction, axis = 0)
-        for latent in np.transpose(dense_latents):
-            predictions.append(neural_net_predict(rating_net_parameters,np.concatenate((row_latent,latent))))
+        rating_predictions[user_index] = np.array(predictions).reshape((rating_indices.sum()))
 
-        rating_predictions[i] = np.array(predictions).reshape((rating_indices.sum()))
+    wipe_caches()
     return rating_predictions #Actual inference
+
+def recurrent_inference(parameters,iter=0,data = None,user_index = 0,movie_index =0):
+    #Predict full matrix
+    rating_net_parameters = parameters[keys_rating_net]
+
+    userLatent = getUserLatent(parameters,data,user_index)
+    movieLatent = getMovieLatent(parameters,data,movie_index)
+    return neural_net_predict(
+            rating_net_parameters,
+            np.concatenate((userLatent
+                            ,movieLatent)))
+
+def getUserLatent(parameters,data,user_index,recursion_depth = MAX_RECURSION, caller_id = -1):
+    #print "user", (MAX_RECURSION-recursion_depth)
+    movie_to_user_net_parameters = parameters[keys_movie_to_user_net]
+    row_size,col_size = data.shape
+    rowLatents = parameters[keys_row_latents]
+    #Check if we should stop
+    if any(USERLATENTCACHE[user_index]):
+        return USERLATENTCACHE[user_index]
+
+    #Check if we already have the latent
+    if user_index < rowLatents.shape[0]:
+        return rowLatents[user_index,:]
+
+    if recursion_depth < 0:
+        return None
+
+    #Must generate latent
+    current_row = data[user_index,:]
+    rating_indices = flatten(current_row > 0)[0] #Only keep indices where the ratings are non-zero
+
+    #dense_ratings = current_row[rating_indices].reshape((1,rating_indices.sum())) #Grab corresponding ratings
+    dense_ratings = []
+    dense_latents = []
+    #print np.flatnonzero(current_row)
+    for movie_index in np.flatnonzero(current_row):
+        if movie_index != caller_id:
+        #Go through all elements of the matrix
+            movie_latent = getMovieLatent(parameters,data,movie_index,recursion_depth-1,user_index)
+            if movie_latent is not None:
+                dense_latents.append(movie_latent) #We got another movie latent
+                dense_ratings.append(current_row[movie_index]) #Add its corresponding rating
+    #Now have all latents
+    #Prepare for concatenations
+    dense_latents = np.transpose(np.array(dense_latents))
+    dense_ratings = np.array(dense_ratings).reshape((1,len(dense_ratings)))
+
+    if (dense_ratings.sum() > 0):
+        latents_with_ratings = np.concatenate((dense_latents,dense_ratings),axis = 0 ) #Append ratings to latents
+        prediction = neural_net_predict(movie_to_user_net_parameters,np.transpose(latents_with_ratings)) #Feed through NN
+        row_latent = np.mean(prediction, axis = 0)
+    else:
+        return None
+
+    USERLATENTCACHE[user_index] = row_latent
+    return row_latent
+
+def getMovieLatent(parameters,data,movie_index,recursion_depth = MAX_RECURSION,caller_id = -1):
+    #print "movie", (MAX_RECURSION-recursion_depth)
+
+    user_to_movie_net_parameters = parameters[keys_user_to_movie_net]
+    row_size, col_size = data.shape
+    colLatents = parameters[keys_col_latents]
+
+    if movie_index<colLatents.shape[1]:
+        return colLatents[:,movie_index]
+
+    if any(MOVIELATENTCACHE[movie_index]):
+        return MOVIELATENTCACHE[movie_index]
+
+    if recursion_depth < 0:
+        return None
+    #Must Generate Latent
+    current_column = data[:,movie_index]
+    rating_indices = flatten(current_column> 0)[0]
+
+    #dense_ratings = current_column[rating_indices].reshape((1,rating_indices.sum()))
+    dense_ratings = []
+    dense_latents = []
+    for user_index in np.flatnonzero(current_column):
+        if user_index != caller_id:
+            user_latent = getUserLatent(parameters,data,user_index,recursion_depth-1,movie_index)
+            if user_latent is not None:
+                dense_latents.append(user_latent)
+                dense_ratings.append(current_column[user_index])
+
+    dense_latents = np.transpose(np.array(dense_latents))
+    dense_ratings = np.array(dense_ratings).reshape((1,len(dense_ratings)))
+
+    if (dense_ratings.sum() > 0):
+        latents_with_ratings = np.concatenate((dense_latents,dense_ratings),axis = 0 ) #Append ratings to latents
+        prediction = neural_net_predict(user_to_movie_net_parameters,np.transpose(latents_with_ratings)) #Feed through NN
+        column_latent = np.transpose(np.mean(prediction, axis = 0))
+    else:
+        return None
+
+    MOVIELATENTCACHE[movie_index] = column_latent
+
+    return column_latent
+
 
 def neural_net_predict(params, inputs):
     """Implements a deep neural network for classification.
@@ -119,8 +223,16 @@ def softmax(x):
 def relu(data):
     return data * (data > 0)
 
+def wipe_caches():
+    global USERLATENTCACHE
+    global MOVIELATENTCACHE
+    USERLATENTCACHE = [np.array((0,0))]*1000
+    MOVIELATENTCACHE = [np.array((0,0))]*1000
+
 inference = neural_net_inference
 loss = nnLoss
 l1_size = 0
 l2_size = 0
-reg_alpha = 0.001
+reg_alpha = .1
+USERLATENTCACHE = [np.array((0,0))]*1000
+MOVIELATENTCACHE = [np.array((0,0))]*1000
--- Framework/utils.py
+++ Framework/utils.py
@@ -1,21 +1,21 @@
 import autograd.numpy as np
-movie_latent_size = 60
+movie_latent_size = 40
 user_latent_size = 40
-hyp_user_network_sizes = [movie_latent_size+1, 60, user_latent_size]
-rating_network_sizes = [movie_latent_size+user_latent_size,50,1]
+hyp_user_network_sizes = [movie_latent_size+1, 100, user_latent_size]
+hyp_movie_network_sizes = [user_latent_size+1,100,movie_latent_size]
+rating_network_sizes = [movie_latent_size+user_latent_size,10,1]
 scale = .1
 
-def build_params(col_size):
-    parameters = []
-    parameters+=(init_random_params(scale,hyp_user_network_sizes))#Neural Net Parameters
-    l1_size = len(parameters)
-    parameters+=(init_random_params(scale,rating_network_sizes))#Neural Net Parameters
-    l2_size = len(parameters)
-    parameters.append(scale * np.random.rand(movie_latent_size,col_size))#Column Latents
-    parameters.append(scale *  np.ones(hyp_user_network_sizes[-1]))#Attention Latent
-    parameters = list(parameters)
+def build_params((row_size,col_size)):
+    parameters = {}
+    parameters[keys_movie_to_user_net]=(init_random_params(scale,hyp_user_network_sizes))#Neural Net Parameters
+    parameters[keys_user_to_movie_net]=(init_random_params(scale,hyp_movie_network_sizes))#Neural Net Parameters
+    parameters[keys_rating_net]=(init_random_params(scale, rating_network_sizes))#Neural Net Parameters
+    parameters[keys_col_latents]=(scale * np.random.rand(movie_latent_size,50))#Column Latents
+    parameters[keys_row_latents]=(scale * np.random.rand(50,user_latent_size))#user_latent_size,row_size))#Row Latents
 
-    return parameters, l1_size, l2_size
+    return parameters
+
 
 #Credit to David Duvenaud for sleek init code
 def init_random_params(scale, layer_sizes, rs=np.random.RandomState(0)):
@@ -24,3 +24,9 @@ def init_random_params(scale, layer_sizes, rs=np.random.RandomState(0)):
     return [(scale * rs.randn(m, n),   # weight matrix
               scale * rs.randn(n))      # bias vector
             for m, n in zip(layer_sizes[:-1], layer_sizes[1:])]
+
+keys_movie_to_user_net = "MovieToUser"
+keys_user_to_movie_net = "UserToMovie"
+keys_row_latents = "RowLatents"
+keys_col_latents = "ColLatents"
+keys_rating_net = "PredNet"
