6a4bb20ca210d7dc531cadc012c19fc35919130f
Calvin Luo <Calvin@Calvins-MacBook-Pro.local>
2017-03-31 19:02:59 -0400

    atnn.py cleaned up

--- Framework/atnn.py
+++ Framework/atnn.py
@@ -8,6 +8,8 @@ from NMF_ATNN import *
 from autograd.optimizers import sgd
 import utils
 from utils import *
+import train
+from train import *
 import NMF_ATNN
 from sklearn.decomposition import NMF
 full_data  = DataLoader().LoadData(file_path="../Data/ml-100k/u.data",data_type=DataLoader.MOVIELENS)
@@ -17,51 +19,23 @@ pre_train, pre_test = full_data[:200,:100], full_data[:400,:100]
 print pre_train
 
 #[Model Parameters
-train_indx = [(np.array(range(200))),(np.array(range(100)))]
-test_idndx = [np.random.choice(range(1000),40),np.random.choice(range(1700),20)]
-
-can_usr_idx, can_mov_idx = get_canonical_indices(full_data, [utils.num_user_latents, utils.num_movie_latents])
-train = fill_in_gaps([can_usr_idx, can_mov_idx], train_indx, full_data)
-test = fill_in_gaps([can_usr_idx, can_mov_idx], test_idndx, full_data)
-
-print "user idx ", can_usr_idx
-print "movie idx", can_mov_idx
+train_user_size = 200
+train_movie_size = 100
 
 #Training Parameters
-num_epochs = 20
-num_iters = 5
+#num_epochs = 20
 step_size = 0.005
+num_iters = 20
 
-parameters = build_params(train.shape)
-#Pretrain rating net and latents
-# train = full_data[np.ix_(can_usr_idx, can_mov_idx)]
-# grads = lossGrad(train)
-# parameters = adam(grads,parameters, step_size=step_size,
-#                         num_iters=num_epochs, callback=dataCallback(train))
-# print "Test performance:"
-# raw_input()
+parameters = build_params([train_user_size + num_user_latents, train_movie_size + num_movie_latents])
 
-# pre training combined
-# train = fill_in_gaps([can_usr_idx, can_mov_idx], [can_usr_idx, can_mov_idx], full_data)
-# zeros = np.zeros((num_user_latents,num_movie_latents))
-# train[:num_user_latents,:num_movie_latents] = np.array(0)
-# train[num_user_latents:,num_movie_latents:] = np.array(0)
-# raw_input()
-# grads = lossGrad(train)
-# print train
-# raw_input()
-# free_params = adam(grads,parameters, step_size=step_size,
-#                         num_iters=num_epochs, callback=dataCallback(train))
-# raw_input()
+parameters = train(full_data, [train_user_size, train_movie_size], parameters, p1=True, p2=True, trainArgs = [step_size, num_iters])
 
-train = fill_in_gaps([can_usr_idx, can_mov_idx], train_indx,full_data)
-grads = lossGrad(train)
-parameters = adam(grads,parameters, step_size=step_size,
-                            num_iters=num_epochs, callback=dataCallback(train))
-#Print performance on each model
+#Print performance on each model - MOVED INTO TRAIN.PY
+'''
 invtrans = getInferredMatrix(parameters,train)
 print "\n".join([str(x) for x in ["Train", print_perf(parameters,data=train), train, np.round(invtrans)]])
 
 invtrans = getInferredMatrix(parameters,test)
 print "\n".join([str(x) for x in ["Test", print_perf(parameters,data=test), test, np.round(invtrans)]])
-
+'''
