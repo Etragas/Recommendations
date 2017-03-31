5e8b558338592af36023bdaa3ea29ef9f062f79d
Elias Tragas <etragas@hotmail.com>
2017-03-31 17:31:57 -0400

    Multiproc

--- Framework/NMF_ATNN.py
+++ Framework/NMF_ATNN.py
@@ -7,29 +7,36 @@ from utils import *
 from autograd.core import primitive
 from autograd.scipy.misc import logsumexp
 from autograd.optimizers import adam
-from joblib import Parallel, delayed
 from multiprocessing.pool import ThreadPool
 import threading
 import time
+import Queue
 from copy import deepcopy
+import sys
+import dill
+import threading
 
 import inspect
-import Queue
-import multiprocessing as mp
-
+import multiprocess as mp
+#import pathos.helpers as mp_vanilla
+from threading import Lock
+from multiprocessing.queues import Queue
 curtime = 0
-MAX_RECURSION = 4
+MAX_RECURSION = 100
 
-movie_cache_lock = threading.Lock()
-user_cache_lock = threading.Lock()
+movie_cache_lock = Lock()
+user_cache_lock = Lock()
+ret_list_lock = Lock()
 rowLatents = 0
 colLatents = 0
 hitcount = 0
+
+ret_list = [[]]
 def lossGrad(data):
     return grad(lambda params,_: nnLoss(params,data=data))
 
 def dataCallback(data):
-    return lambda x,iter,grad: print_perf(x,iter,grad,data=data)
+    return lambda params,iter,grad: print_perf(params,iter,grad,data=data)
 
 def nnLoss(parameters,iter=0,data=None):
     """
@@ -67,6 +74,9 @@ def getInferredMatrix(parameters,data):
         newarray[i,ratings_high] = inferred[i]
     return newarray
 
+def run_dill_encoded(payload):
+    fun, args = dill.loads(payload)
+    return fun(*args)
 
 def print_perf(params, iter=0, gradient={}, data = None):
     """
@@ -94,16 +104,47 @@ def neural_net_inference(parameters,iter = 0, data = None):
     :return: A list of numpy arrays, each of which corresponds to a dense prediction of user ratings.
     """
     rating_predictions = [0]*data.shape[0]
+    global ret_list
+    print sys.getrecursionlimit()
+    sys.setrecursionlimit(50000)
     global hitcount
     hitcount = 0
-
     num_rows, num_columns = data.shape
-    half1 = get_pred_for_users(parameters,data,np.array(range(num_rows/2)))
-    half2 = get_pred_for_users(parameters,data,np.array(range(num_rows/2,num_rows)))
-    wipe_caches()
-    return half1+half2
+    num_cpu = mp.cpu_count()
+    ret_list = []
+    #Will use ques for implicit ordering
+    #m = mp.Manager()
+    ranges = disseminate_values(num_rows,num_cpu)
+    threads = []
+    prev_pos = 0
+    for x in range(num_cpu) :
+        data_range = range(prev_pos,prev_pos+ranges[x])
+        prev_pos += ranges[x]
+        t = threading.Thread(target=get_pred_for_users,args=(parameters,data,data_range,))
+        threads.append(t)
+        t.start()
+    for t in threads:
+        t.join()
 
-def get_pred_for_users(parameters,data,user_indices):
+#    results = [result_queue.get() for x in range(num_cpu)]
+    sorts = sorted(ret_list,key = lambda tup: tup[0])
+
+    # half1 = get_pred_for_users(parameters,data,np.array(range(num_rows/2)))
+    # half2 = get_pred_for_users(parameters,data,np.array(range(num_rows/2,num_rows)))
+    ratings = reduce(lambda init , new: init + new[1],sorts,[])
+    wipe_caches()
+    return ratings
+
+def disseminate_values(num_items,num_bins):
+    chunk_size = num_items/num_bins
+    ranges = [chunk_size]*num_bins
+    remainder = num_items-num_bins*chunk_size
+    #Remainder < num_bins
+    for x in range(remainder):
+        ranges[x] += 1
+    return ranges
+
+def get_pred_for_users(parameters,data,user_indices, queue = None):
     rating_predictions = []
 
     for user_index in user_indices:
@@ -119,6 +160,12 @@ def get_pred_for_users(parameters,data,user_indices):
             predictions.append(recurrent_inference(parameters,iter,data,user_index,rating_index))
 
         rating_predictions.append(np.array(predictions).reshape((rating_indices.sum())))
+
+    if queue:
+        queue.put((user_indices[0],rating_predictions))
+        return
+    with ret_list_lock:
+        ret_list.append((user_indices[0],rating_predictions))
     return rating_predictions
 
 def recurrent_inference(parameters,iter=0,data = None,user_index = 0,movie_index =0):
@@ -127,6 +174,7 @@ def recurrent_inference(parameters,iter=0,data = None,user_index = 0,movie_index
 
     userLatent = getUserLatent(parameters,data,user_index)
     movieLatent = getMovieLatent(parameters,data,movie_index)
+
     return neural_net_predict(
             rating_net_parameters,
             np.concatenate((userLatent
@@ -135,10 +183,11 @@ def recurrent_inference(parameters,iter=0,data = None,user_index = 0,movie_index
 def getUserLatent(parameters,data,user_index,recursion_depth = MAX_RECURSION, caller_id = [[],[]]):
     movie_to_user_net_parameters = parameters[keys_movie_to_user_net]
     rowLatents = parameters[keys_row_latents]
-    
+
     #Check if latent is cached
     if any(USERLATENTCACHE[user_index]):
         global hitcount
+
         hitcount += 1
 
         return USERLATENTCACHE[user_index]
@@ -175,8 +224,9 @@ def getUserLatent(parameters,data,user_index,recursion_depth = MAX_RECURSION, ca
         row_latent = np.mean(prediction, axis = 0)
     else:
         return None
+    with user_cache_lock:
+        USERLATENTCACHE[user_index] = row_latent
 
-    USERLATENTCACHE[user_index] = row_latent
     return row_latent
 
 def getMovieLatent(parameters,data,movie_index,recursion_depth = MAX_RECURSION, caller_id = [[],[]]):
@@ -221,7 +271,8 @@ def getMovieLatent(parameters,data,movie_index,recursion_depth = MAX_RECURSION, 
     else:
         return None
 
-    MOVIELATENTCACHE[movie_index] = column_latent
+    with movie_cache_lock:
+        MOVIELATENTCACHE[movie_index] = column_latent
 
     return column_latent
 
--- Framework/atnn.py
+++ Framework/atnn.py
@@ -11,16 +11,15 @@ from utils import *
 import NMF_ATNN
 from sklearn.decomposition import NMF
 full_data  = DataLoader().LoadData(file_path="../Data/ml-100k/u.data",data_type=DataLoader.MOVIELENS)
-
+full_data = full_data[:1000,:1700]
 #Reduce the matrix to toy size
 pre_train, pre_test = full_data[:200,:100], full_data[:400,:100]
-print pre_train
+
 #[Model Parameters
 train_indx = [(np.array(range(200))),(np.array(range(100)))]
-test_idndx = [np.array(range(200,240)),np.array(range(100,140))]
+test_idndx = [np.random.choice(range(1000),40),np.random.choice(range(1700),20)]
 
 can_usr_idx, can_mov_idx = get_canonical_indices(full_data, [utils.num_user_latents, utils.num_movie_latents])
-
 train = fill_in_gaps([can_usr_idx, can_mov_idx], train_indx, full_data)
 test = fill_in_gaps([can_usr_idx, can_mov_idx], test_idndx, full_data)
 
@@ -31,21 +30,12 @@ print "movie idx", can_mov_idx
 num_epochs = 20
 num_iters = 5
 step_size = 0.005
-train = full_data[np.ix_(can_usr_idx, can_mov_idx)]
 
 parameters = build_params(train.shape)
-
+#Pretrain rating net and latents
 grads = lossGrad(train)
-
 parameters = adam(grads,parameters, step_size=step_size,
                         num_iters=num_epochs, callback=dataCallback(train))
-print "Test performance:"
-print_perf(parameters,data=test)
-
-train = fill_in_gaps([can_usr_idx, can_mov_idx], train_indx, full_data)
-grads = lossGrad(train)
-parameters = adam(grads,parameters, step_size=step_size,
-                            num_iters=num_epochs, callback=dataCallback(train))
 #Print performance on each model
 invtrans = getInferredMatrix(parameters,train)
 print "\n".join([str(x) for x in ["Train", print_perf(parameters,data=train), train, np.round(invtrans)]])
