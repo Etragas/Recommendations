9a4783829eb628858f4254804dad9c21f2911050
Elias Tragas <etragas@snapchat.com>
2017-10-21 03:11:23 -0400

    makes 100

--- Framework/atnn.py
+++ Framework/atnn.py
@@ -1,7 +1,8 @@
+import pickle
+
 import numpy as np
 import torch
 import utils
-import pickle
 from DataLoader import *
 from train import *
 from utils import build_params, keys_rating_net, keys_row_latents, keys_col_latents, get_canonical_indices, \
@@ -16,7 +17,7 @@ full_data = DataLoader().LoadData(file_path="../Data/ml-100k/u.data", data_type=
 print(full_data.shape)
 print(np.mean(np.sum(full_data > 0, axis=1)))  # Check average ratings per user
 # Reduce the matrix to toy size
-full_data = full_data[:100,:100]
+# full_data = full_data[:100,:100]
 rows = [x for x in range((full_data.shape[0])) if full_data[x, :].sum() > 0]
 cols = [x for x in range((full_data.shape[1])) if full_data[:, x].sum() > 0]
 full_data = full_data[rows, :]
@@ -48,7 +49,7 @@ batches_per_can_epoch = int(np.ceil(float(utils.num_user_latents) / num_users_pe
 num_epochs = 40
 hyperp1 = [step_size * 10, 40, batches_per_can_epoch]
 hyperp2 = [step_size * 10, 40, batches_per_can_epoch]
-#hypert = [step_size, num_epochs, batches_per_epoch]
+# hypert = [step_size, num_epochs, batches_per_epoch]
 hypert = [1, 1, 2]
 
 # Build the dictionary of parameters for the nets, etc.
